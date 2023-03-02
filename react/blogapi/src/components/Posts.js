e6759becc325f5291a4063b910339fc5c9e4b0d4
rmendes1 <joaorenatomendes@gmail.com>
2023-03-02 15:13:32 -0300

    fix(react): Posts component
    fixing imports that were missed

--- react/blogapi/src/components/Posts.js
+++ react/blogapi/src/components/Posts.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 
 const useStyles = makeStyles((theme) => ({
