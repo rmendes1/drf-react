e1c686681ed37919d61dfa478f7aa1698501b5d7
rmendes1 <joaorenatomendes@gmail.com>
2023-03-02 14:15:28 -0300

    test: test class for Post model created. setUpTestData mounted to start testing functionalities

--- django/blog/tests.py
+++ django/blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User 
+from django.utils import timezone
+from blog.models import Post, Category 
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    @classmethod 
+    def setUpTestData(cls):
+        testcategory = Category.objects.create(name='django')
+        testauthor = User.objects.create_user(username='testuser', password='testpass')
+        Post.objects.create(
+            category_id=1,
+            title='Test Title',
+            excerpt='Test Excerpt',
+            content='Test Content',
+            slug='test-title',
+            published=timezone.now(),
+            author_id=1,
+            status='published'
+        )
