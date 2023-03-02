d92710beb095bddfc9d47905a8222b7106ff0893
rmendes1 <joaorenatomendes@gmail.com>
2023-03-02 15:11:31 -0300

    feat(react): Posts component created
    Material UI config added

---
+++ react/blogapi/src/components/Posts.js
@@ -0,0 +1,29 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+	cardMedia: {
+		paddingTop: '56.25%', // 16:9
+	},
+	link: {
+		margin: theme.spacing(1, 1.5),
+	},
+	cardHeader: {
+		backgroundColor:
+			theme.palette.type === 'light'
+				? theme.palette.grey[200]
+				: theme.palette.grey[700],
+	},
+	postTitle: {
+		fontSize: '16px',
+		textAlign: 'left',
+	},
+	postText: {
+		display: 'flex',
+		justifyContent: 'left',
+		alignItems: 'baseline',
+		fontSize: '12px',
+		textAlign: 'left',
+		marginBottom: theme.spacing(2),
+	},
+}));
