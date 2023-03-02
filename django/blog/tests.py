68b26a46e4f9225b860b19c4f055c1fe576d61b3
rmendes1 <joaorenatomendes@gmail.com>
2023-03-02 14:17:30 -0300

    test: testing title max-length and status choices on PostModelTest

--- django/blog/tests.py
+++ django/blog/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User 
 from django.utils import timezone
 from blog.models import Post, Category 
+from django.core.exceptions import ValidationError
 
 
 class PostModelTest(TestCase):
@@ -35,3 +36,15 @@ class PostModelTest(TestCase):
         self.assertEqual(status, 'published')
         self.assertEqual(str(post), 'Test Title') # test model __str__
         self.assertEqual(str(cat), 'django')
+
+    def test_title_max_length(self):
+        post = Post.objects.get(id=1)
+        max_length = post._meta.get_field('title').max_length
+        self.assertEquals(max_length, 250)
+
+    def test_status_choices(self):
+        post = Post.objects.get(id=1)
+        invalid_status = 'invalid'
+        post.status = invalid_status
+        with self.assertRaises(ValidationError):
+            post.full_clean()
