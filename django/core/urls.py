9ccb2330130e2b63e950283c3932ac9d0ace561f
rmendes1 <joaorenatomendes@gmail.com>
2023-03-07 20:47:34 -0300

    feat(core): added path api-auth
    this path is provided by DRF to enable login inside /api

--- django/core/urls.py
+++ django/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('api/', include('blogapi.urls', namespace='blogapi')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
