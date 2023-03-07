a71a1a69b5396fdd2dc25fe0e270c809ace078c5
rmendes1 <joaorenatomendes@gmail.com>
2023-03-07 20:44:34 -0300

    test(blogapi): added test_post_update function; updated test_view_posts
    It tests if different authors can edit a post by anther author who created it;
    Changed client to APIClient to login before testcase

--- django/blogapi/tests.py
+++ django/blogapi/tests.py
@@ -4,14 +4,19 @@ from rest_framework import status
 from rest_framework.test import APITestCase 
 from blog.models import Post, Category 
 from django.contrib.auth.models import User 
+from rest_framework.test import APIClient
 
 class PostTests(APITestCase):
 
     def test_view_posts(self):
-
+        """Tests if a user can see the posts list"""
+        client = APIClient()
+        self.testuser1 = User.objects.create_user(username='testuser1', password='1234567')
+        client.login(username=self.testuser1.username, password='1234567')
         url = reverse('blogapi:listcreate')
-        response = self.client.get(url, format='json')
+        response = client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     
     def create_post(self):
@@ -24,6 +29,56 @@ class PostTests(APITestCase):
                 "content": "new"
                 }
 
-        url = reverse('blog_api:listcreate')
+        url = reverse('blogapi:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_post_update(self):
+        """Tests if a different user can edit a post from another author"""
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(username='testuser1', password='1234567') # this is going to be the post author
+        self.testuser2 = User.objects.create_user(username='testuser2', password='12345678') # this is to test if this user can change the post
+
+        Post.objects.create(
+            category_id=1,
+            title='Post Title',
+            excerpt='Excerpt 1',
+            content='Content 1',
+            author_id=1,
+            status='published',
+        )
+
+        # testing user1
+        client.login(username=self.testuser1.username, password='1234567')
+
+        url = reverse(('blogapi:detailcreate'), kwargs={"pk": 1})
+
+        response = client.put(
+            url, {
+                "id": 1,
+                "title": "New",
+                "author": 1,
+                "excerpt": "New",
+                "content": "New",
+                "status": "published"
+            }, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # testing user2
+        client.login(username=self.testuser2.username, password='12345678')
+
+        response = client.put(
+            url, {
+                "id": 1,
+                "title": "New",
+                "author": 1,
+                "excerpt": "New",
+                "content": "New",
+                "status": "published"
+            }, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
