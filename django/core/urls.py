35972ea3cca130c90e47e758dd2835337ffed13d
rmendes1 <joaorenatomendes@gmail.com>
2023-03-11 14:56:29 -0300

    fix(core-urls): token urls adjusted

--- django/core/urls.py
+++ django/core/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('api/', include('blogapi.urls', namespace='blogapi')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
