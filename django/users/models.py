2dd2c2aaa721bbf88ecadd6149523d1698f2be06
rmendes1 <joaorenatomendes@gmail.com>
2023-03-11 12:01:33 -0300

    feat(users): class for custom account manager added

--- django/users/models.py
+++ django/users/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.'
+            )
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.'
+            )
+        
+        return self.create_user(email, user_name, first_name, password, **other_fields)
